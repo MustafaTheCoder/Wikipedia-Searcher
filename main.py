4f9714b6cda9e4dc207af2d69aa56d71b3a29df7
MustafaTheCoder <73116655+MustafaTheCoder@users.noreply.github.com>
2021-09-20 17:17:46 +0500

    Added Algorithm

---
+++ main.py
@@ -0,0 +1,27 @@
+import pyttsx3
+import wikipedia
+
+engine = pyttsx3.init()
+
+def speak(arg):
+    rate = engine.getProperty('rate')
+    volume = engine.getProperty('volume')
+    voices = engine.getProperty('voices')
+
+    engine.setProperty('rate', 160)
+    engine.setProperty('volume', 2)
+    engine.setProperty('voice', voices[0].id)
+
+    engine.say(arg)
+    engine.runAndWait()
+
+
+while True:
+    user_inp = input("> ")
+    speak('Searching Wikipedia...')
+    results = wikipedia.summary(user_inp, sentences=2)
+    print(f"Searching: {user_inp}")
+    speak("According to Wikipedia")
+    print(f"Result: {results}")
+    speak(results)
+
